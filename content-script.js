ac58e9e29df1728930afb03b756575336dc8b401
Valdar Vaht <valdars@gmail.com>
2018-02-05 00:25:47 +0200

    New approach

--- content-script.js
+++ content-script.js
@@ -11,8 +11,9 @@ $form.append($prepareImportButton);
 let $startImportButton = $('<button type="submit" class="btn btn-default hidden"><span class="fas fa-upload fa-fw" aria-hidden="true" title=""></span> <span class="span-1280">Start importing</span></button>');
 $form.append($startImportButton);
 
-$fetchJsonButton.on('click', function(evt) {
+$fetchJsonButton.on('click', function (evt) {
     evt.preventDefault();
+
     var request = new window.XMLHttpRequest();
     request.overrideMimeType("application/json");
     request.addEventListener("load", function () {
@@ -24,16 +25,21 @@ $fetchJsonButton.on('click', function(evt) {
     });
     request.open("GET", "https://vatoto.com/follows_export?method=json");
     request.send();
-    
+
     return false;
 });
 
-$prepareImportButton.on('click', function(evt) {
+$prepareImportButton.on('click', function (evt) {
     evt.preventDefault();
 
     let $list = $('<ul class="list-group">');
 
-    var json = JSON.parse($textarea.val());
+    var json = parseAndValidateJson($textarea.val());
+    if(!json) {
+        alert('Invalid JSON');
+        return false;
+    }
+
     follows = [];
     json.forEach(x => {
         follows.push(x.comic_id);
@@ -41,20 +47,45 @@ $prepareImportButton.on('click', function(evt) {
     });
     $textarea.replaceWith($list);
 
+    $fetchJsonButton.addClass('hidden');
     $prepareImportButton.addClass('hidden');
     $startImportButton.removeClass('hidden');
 
     //importFollow(22832);
-    
+
     return false;
 });
 
-$startImportButton.on('click', function(evt) {
+$startImportButton.on('click', function (evt) {
     evt.preventDefault();
 
     setInterval(importNextFollow, 1000);
     return false;
 });
+
+function parseAndValidateJson(text) {
+    let json = null;
+    try {
+        json = JSON.parse(text);
+    }catch(ex) {
+        return false;
+    }
+    
+    if (!Array.isArray(json)) {
+        return false;
+    }
+
+    if (json.length == 0) {
+        return false;
+    }
+
+    let follow = json[0];
+    if (follow == null || typeof follow !== 'object' || !follow.comic_id || !follow.title) {
+        return false;
+    }
+
+    return json;
+}
 
 function importNextFollow() {
     let followId = follows.shift();
@@ -64,15 +95,18 @@ function importNextFollow() {
 function importFollow(id) {
     let $status = $(`.follow-status[data-followid="${id}"]`);
     $status.text('Processing').removeClass('label-info').addClass('label-primary');
-    setTimeout(function() {
-        $status.text('Done').removeClass('label-primary').addClass('label-success');
-    }, 500);
-    return;
-    $.ajax({
+    let request = $.ajax({
         url: `/ajax/actions.ajax.php?function=manga_follow&id=${id}`,
         type: "GET",
         xhr: () => {
             return new content.XMLHttpRequest();
         }
+    });
+    request.done(function (msg) {
+        $status.text('Done').removeClass('label-primary').addClass('label-success');
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        $status.text('Failed').removeClass('label-primary').addClass('label-danger');
     });
 }
